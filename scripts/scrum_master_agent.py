1696c77497adcf090d3ece6ee563431ef30fc390
oleg <osavelyev@gmail.com>
2025-12-30 10:03:10 +0200

    Update scrum_master_agent.py

--- scripts/scrum_master_agent.py
+++ scripts/scrum_master_agent.py
@@ -88,7 +88,7 @@ Instructions:
     
     try:
         response = client.chat.completions.create(
-            model='gpt-4',
+            model='gpt-3.5-turbo',
             messages=[{'role': 'user', 'content': prompt}],
             temperature=0.7
         )
